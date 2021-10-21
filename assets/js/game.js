9ceaf620febd984b3d339dc994d70a1440aafcbd
Donald Biggers <DuGsEmail@yahoo.com>
2021-10-21 16:17:36 -0500

    fight-skip prompt response handling

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,33 @@
+var fightOrSkip = function() {
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  //conditional recursive function call
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+
+  promptFight = promptFight.toLocaleLowerCase();
+
+  if (promptFight === "skip") {
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+      playerInfo.playerMoney = Math.max(0, playerInfo.money - 10);
+      shop();
+      return true;
+    }
+  }
+  return false;
+}
 var fight = function(enemy) {
   console.log(playerInfo.health)
   console.log(enemy.health)
   while (playerInfo.health > 0 && enemy.health > 0) {
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-
-    
-    if (promptFight === "skip" || promptFight === "SKIP") {
-
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-
-      if (confirmSkip) {
-        window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-        playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money);
-        break;
-      }
+    if (fightOrSkip()) {
+      break;
     }
-
+    var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     enemy.health = Math.max(0, enemy.health - damage);
     console.log(
